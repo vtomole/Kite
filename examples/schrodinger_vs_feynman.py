c398b7cf8b9d3c071247d2a5f62cfa678b5049d5
vtomole <victory.omole@kingland.com>
2020-10-06 13:03:38 -0500

    Replace travis with github actions

--- examples/schrodinger_vs_feynman.py
+++ examples/schrodinger_vs_feynman.py
@@ -1,17 +1,22 @@
 import kite as kt
 
-prog = kt.Program(
-    kt.QREG(2, probability_of_amplitude='00'),
-    kt.H(0),
-    kt.CNOT(0,1))
+def main():
+    "To demonstrate the implementation and equivalence of results from the Schrodinger and Feynman simulators"
+    prog = kt.Program(
+        kt.QREG(2, probability_of_amplitude='00'),
+        kt.H(0),
+        kt.CNOT(0,1))
 
-amplitude_from_schrodinger = kt.schrodinger(prog)
-print("Amplitude from Schrodinger")
-print(amplitude_from_schrodinger )
+    amplitude_from_schrodinger = kt.schrodinger(prog)
+    print("Amplitude from Schrodinger")
+    print(amplitude_from_schrodinger )
 
 
-amplitude_from_feynman = kt.feynman(prog)
-print("Amplitude from Feynman")
-print(amplitude_from_feynman)
+    amplitude_from_feynman = kt.feynman(prog)
+    print("Amplitude from Feynman")
+    print(amplitude_from_feynman)
 
-assert amplitude_from_schrodinger == amplitude_from_feynman
+    assert amplitude_from_schrodinger == amplitude_from_feynman
+
+if __name__ == '__main__':
+    main()
